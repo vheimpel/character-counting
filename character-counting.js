0916ee1068749c7f0e86ef5b9a7ef9fb822451a6
vheimpel <vheimpel@gmail.com>
2017-05-03 17:18:05 +0000

    No spaces

--- character-counting.js
+++ character-counting.js
@@ -4,15 +4,26 @@
 //TODO: Compare letters, if letter[i] !=== letter[i]+1 return letter
 //TODO: Count the letters somehow
 
+// function countLetters(word) {
+//   var letter = word.split
+//   for (letter of word) {
+//     console.log(letter)
+//   }
+// }
+
+// var noSpaces = word.split(" ").join("");
+
 var word = process.argv[2];
+// var word = "Lighthouse Labs";
 
-function countLetters(word) {
-  var letter = word.split
-  for (letter of word) {
-    console.log(letter)
-  }
+wordNoSpace = word.replace(/\s/g,'');
+var countLetters={}
+var repeats=[];
 
+for(i = 0, length = wordNoSpace.length; i < length; i++) {
+    var l = wordNoSpace.charAt(i)
+    countLetters[l] = (isNaN(countLetters[l]) ? 1 : countLetters[l] + 1);
 }
 
-countLetters(word)
-
+console.log(wordNoSpace)
+console.log(countLetters)
