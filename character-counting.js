226f9d3df1eaf4c92c24141c9f75fc6f9a009ba3
vheimpel <vheimpel@gmail.com>
2017-05-03 15:45:44 +0000

    Test commit

---
+++ character-counting.js
@@ -0,0 +1,18 @@
+//TODO: Get input for command line?
+//TODO: Split the string into individual letters?
+//TODO: Loop through each letter
+//TODO: Compare letters, if letter[i] !=== letter[i]+1 return letter
+//TODO: Count the letters somehow
+
+var word = process.argv[2];
+
+function countLetters(word) {
+  var letter = word.split
+  for (letter of word) {
+    console.log(letter)
+  }
+
+}
+
+countLetters(word)
+
